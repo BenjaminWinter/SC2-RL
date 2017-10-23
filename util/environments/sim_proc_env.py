309b9e74af90fa21b0c6d74a54584629b152a4b7
Benjamin Winter <Benjamin.Winter@e-pixler.de>
2017-10-23 17:29:59 +0200

    add sim proc environment

---
+++ util/environments/sim_proc_env.py
@@ -0,0 +1,16 @@
+import util.helpers as helpers
+
+class SimProcEnv():
+    def __init__(self, render=False):
+        self._env = helpers.get_env_wrapper(render)
+        self.observation_space = self._env.observation_space
+        self.action_space = self._env.action_space
+        self.num_envs = 1
+    def step(self, actions):
+        obs,r,done, _ = self._env.step(actions[0])
+        return [obs], [r], [done], [_]
+    def reset(self):
+        obs = self._env.reset()
+        return [obs]
+    def close(self):
+        return self._env.close()
