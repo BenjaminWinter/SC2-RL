a6498fd84ecf79881c5c12340e2910c08b79cd88
Benjamin Winter <Benny.Winter@yahoo.de>
2018-01-31 17:22:02 +0100

    Fix various bugs

--- convert_replays.py
+++ convert_replays.py
@@ -1,17 +1,13 @@
-import json, argparse, glob, os, math
+import glob, os, gzip, json
 import numpy as np
 
 from pysc2 import maps
 from pysc2 import run_configs
-from pysc2.env import sc2_env
-from pysc2.lib import renderer_human
-from pysc2.lib import stopwatch
 from pysc2.lib import features, actions
 from pysc2.lib.protocol import ProtocolError
 from a3c.action_args.agent import Agent
 
 from util import helpers
-import multiprocessing as mp
 import maps.scenarios as scenarios
 import mpyq
 import six
@@ -24,11 +20,21 @@ from absl import app
 from absl import flags
 from s2clientprotocol import sc2api_pb2 as sc_pb
 
-from absl import flags
-
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('dir', 'logs/replays', 'replaydirectory')
+flags.DEFINE_bool('raw_rewards', False, 'use environment rewards instead of n-step-rewards')
+
+
+class FakeQueue():
+    def __init__(self):
+        self._arr = []
+
+    def put(self, el):
+        self._arr.append(el)
+
+    def get(self):
+        return self._arr
 
 
 def main(argv):
@@ -45,7 +51,7 @@ def main(argv):
     interface.feature_layer.minimap_resolution.x = 64
     interface.feature_layer.minimap_resolution.y = 64
 
-    queue = mp.Queue()
+    queue = FakeQueue()
     #
     shared.gamma_n = FLAGS.gamma ** FLAGS.n_step_return
     env = helpers.get_env_wrapper(False)
@@ -54,7 +60,6 @@ def main(argv):
     none_state = np.zeros(s_space)
     none_state = none_state.reshape(s_space)
     replay_agent = Agent(env.action_space.n, t_queue=queue, none_state=none_state)
-
 
     for fname in glob.glob(os.path.join(FLAGS.dir, '*.SC2Replay')):
         replay_data = run_config.replay_data(fname)
@@ -76,15 +81,16 @@ def main(argv):
             results = 0
             last_reward = 0
             while True:
-                # controller.step(FLAGS.step_mul)
-                # obs = controller.observe()
-                # continue
-
                 actions = []
                 for a in obs.actions:
                     try:
                         temp = feat.reverse_action(a)
-                        actions.append([env._actions.index(temp[0]), temp.arguments[1][0], temp.arguments[1][1]])
+                        x = 0
+                        y = 0
+                        if temp[0] not in [0, 7]:
+                            x = temp.arguments[1][0]
+                            y = temp.arguments[1][1]
+                        actions.append([env._actions.index(temp[0]), x ,y])
                     except ValueError:
                         pass
 
@@ -108,32 +114,27 @@ def main(argv):
                 if r == 0 and last_reward != 0:
                     s_ = None
                     print('Episode end')
-                    last_reward = 0
 
-                replay_agent.train(s, actions[0][0], actions[0][1], actions[0][2], r, s_)
+                results += 1
+
+                if not FLAGS.raw_rewards:
+                    replay_agent.train(s, actions[0][0], actions[0][1], actions[0][2], r, s_)
+                else:
+                    queue.put([s, actions[0][0], actions[0][1], actions[0][2], r, s_])
 
                 if obs.player_result:
                     break
+
+                if r == 0 and last_reward != 0:
+                    last_reward = 0
                 else:
                     s = s_
                     last_reward = r
 
-    with open('./replay_info/info.json', 'wb+') as outfile:
-        obj = []
-        while not queue.empty():
-            #temp = queue.get()
-            if s is None:
-                continue
-
-            obj.append(queue.get())
-            # s_ = None
-            # if temp[5] is not None:
-            #     s_ = temp[5].tolist()
-
-            #obj.append([temp[0].tolist(), temp[1].tolist(), temp[2].tolist(), temp[3].tolist(), temp[4], s_])
-
-        pickle.dump(obj, outfile)
-        #json.dump(obj, outfile)
+    with gzip.open('./replay_info/info.gz', 'wb+') as outfile:
+        print('pushed: {}'.format(results))
+        #json.dump(queue.get(), outfile)
+        pickle.dump(queue.get(), outfile)
 
 sf = features.SCREEN_FEATURES
 FEATURE_IDS = {
@@ -152,8 +153,8 @@ FEATURE_IDS = {
     sf.effects.index : 'effects'
 }
 TYPES = {
-    8 : 'c',
-    32 : 'i'
+    8 : 'i1',
+    32 : 'i4'
 }
 
 
